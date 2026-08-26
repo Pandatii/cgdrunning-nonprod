9b66db3706dfb490b229b9ccd6c89b74f8df7f0e
panda <pandatii@gmail.com>
2026-08-27 00:23:44 +0700

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import './index.css'
 
 // ใส่ Link ที่เตรียมไว้ตรงนี้
-const TARGET_URL = 'https://script.google.com/macros/s/AKfycbzdM-Enuu-i4upy2DCSx3jx6htc6qGrbvwKUHTCSLECvd1JhPPGU2F8b_uFhn2-YEIn/exec';
+const TARGET_URL = 'https://script.google.com/macros/s/AKfycbzv4l10GhCW0_a2davh-SpGpyyeAOAh6zHxVK29HjvA40ztX_4gcsyX1vukvxxOeZv0/exec';
 
 // สั่ง Redirect ทันที
 window.location.replace(TARGET_URL);
