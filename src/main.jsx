edfae5a5a7437fe02bff1cefe8e3d70f813d97fc
panda <pandatii@gmail.com>
2026-08-31 15:06:39 +0700

    fixed bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import './index.css'
 
 // ใส่ Link ที่เตรียมไว้ตรงนี้
-const TARGET_URL = 'https://script.google.com/macros/s/AKfycbyThrjF1I7Z4dwELkkhpsDcPRFAORF4Ba6lkgLwKC38L1ZX9vZG-FcN0NVgM1Puy-In/exec';
+const TARGET_URL = 'https://script.google.com/macros/s/AKfycbyKx4JJiBvKaiIzLEXbNzFUIieVmr9ZYVh1_VIljeJv7oGjgqLggkLMOXenL6lGrYdp/exec';
 
 // สั่ง Redirect ทันที
 window.location.replace(TARGET_URL);
