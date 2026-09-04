46b7c03cb74b1d3c6bec5b542522017bc4d648f2
panda <pandatii@gmail.com>
2026-09-04 17:23:34 +0700

    fixed bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import './index.css'
 
 // ใส่ Link ที่เตรียมไว้ตรงนี้
-const TARGET_URL = 'https://script.google.com/macros/s/AKfycbybcIaXitBu2rOuoQcdcs568GIjVsu1PAHnCqUe4Zju_HmBtZU5Ql2IiNDSpwVXwpAv/exec';
+const TARGET_URL = 'https://script.google.com/macros/s/AKfycbzdUzRWjxtCLW32UtbWjEk3vLui82GRWRITY4csv1JwM9HcL4DW3NyZVJoHSKH8COmx/exec';
 
 // สั่ง Redirect ทันที
 window.location.replace(TARGET_URL);
